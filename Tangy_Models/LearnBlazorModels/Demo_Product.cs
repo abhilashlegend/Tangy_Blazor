0b8495ea941b48413f6e62a8bcff4e9da6ec43fa
abhilashlegend <abhilash456a@live.com>
2023-01-06 13:16:09 +0530

    added products

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -15,7 +15,7 @@ namespace Tangy_Models.LearnBlazorModels
 
         public bool IsActive { get; set; }
 
-        public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
+        public List<Demo_ProductProp> ProductProperties { get; set; }
 
     }
 }
