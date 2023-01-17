845a94aab4b397732dd7ad642f56420cf62918ec
abhilashlegend <abhilash456a@live.com>
2023-01-17 22:03:32 +0530

    Application DB Context setup

---
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_DataAccess
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
