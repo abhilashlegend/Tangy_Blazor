81fc563ec8497b398f09acd03fa366c96dd73b13
abhilashlegend <abhilash456a@live.com>
2023-01-19 21:32:11 +0530

    form validation

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Tangy_Models
     public class CategoryDTO
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name is required")]
         public string Name { get; set; }
     }
 }
