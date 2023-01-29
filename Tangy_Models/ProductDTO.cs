747e950c5dd7340aa467c7a3b223e1bb13985a9c
abhilashlegend <abhilash456a@live.com>
2023-01-29 13:32:16 +0530

    ProductDTO created

---
+++ Tangy_Models/ProductDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+        public bool ShopFavourites { get; set; }
+        public bool CustomerFavourites { get; set; }
+
+        public string Color { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category")]
+        public int CategoryId { get; set; }
+        public CategoryDTO Category { get; set; }
+
+    }
+}
