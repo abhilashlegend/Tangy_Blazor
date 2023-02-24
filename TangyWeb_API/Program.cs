1229078bd6dc51aa32b3fc84e72b9999aec2b795
abhilashlegend <abhilash456a@live.com>
2023-02-24 19:57:49 +0530

    Added Controller class and dependency injection of productRepo

--- TangyWeb_API/Program.cs
+++ TangyWeb_API/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Tangy_Business.Repository.iRepository;
+using Tangy_Business.Repository;
 using Tangy_DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
