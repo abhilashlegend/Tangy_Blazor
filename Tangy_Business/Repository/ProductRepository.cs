ecd45350b34fc3038eb53aeab44d2f39ffd9cf0d
abhilashlegend <abhilash456a@live.com>
2023-02-24 21:49:49 +0530

    Added product prices to include in repo

--- Tangy_Business/Repository/ProductRepository.cs
+++ Tangy_Business/Repository/ProductRepository.cs
@@ -47,7 +47,7 @@ namespace Tangy_Business.Repository
 
         public async Task<ProductDTO> Get(int id)
         {
-            var obj = await _db.Products.Include(u => u.Category).FirstOrDefaultAsync(p => p.Id == id);
+            var obj = await _db.Products.Include(u => u.Category).Include(u => u.ProductPrices).FirstOrDefaultAsync(p => p.Id == id);
             if(obj != null)
             {
                 return _mapper.Map<Product, ProductDTO>(obj);
@@ -57,7 +57,7 @@ namespace Tangy_Business.Repository
 
         public async Task<IEnumerable<ProductDTO>> GetAll()
         {
-            return _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDTO>>(_db.Products.Include(u => u.Category));
+            return _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDTO>>(_db.Products.Include(u => u.Category).Include(u => u.ProductPrices));
         }
 
         public async Task<ProductDTO> Update(ProductDTO objDTO)
