e50af625c631b4b45943876910c74d7d295b1af8
abhilashlegend <abhilash456a@live.com>
2023-03-08 22:12:01 +0530

    implemented cart service

---
+++ TangyWeb_Client/Service/IService/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using TangyWeb_Client.ViewModels;
+
+namespace TangyWeb_Client.Service.IService
+{
+    public interface ICartService
+    {
+        Task DecrementCart(ShoppingCart shoppingCart);
+
+        Task IncrementCart(ShoppingCart shoppingCart);
+    }
+}
