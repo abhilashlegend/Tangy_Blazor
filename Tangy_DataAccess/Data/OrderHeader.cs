33552936429eaac116fbf3947139c89dcfac1c52
abhilashlegend <abhilash456a@live.com>
2023-04-19 18:58:24 +0530

    order summary ui done

--- Tangy_DataAccess/Data/OrderHeader.cs
+++ Tangy_DataAccess/Data/OrderHeader.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tangy_DataAccess.Data
+namespace Tangy_DataAccess
 {
     public class OrderHeader
     {
@@ -14,25 +14,40 @@ namespace Tangy_DataAccess.Data
 
         [Required]
         public string UserId { get; set; }
+        // add nagivation property : #TODO
 
         [Required]
-        [Display(Name = "Order Total")]
         public double OrderTotal { get; set; }
 
         [Required]
         public DateTime OrderDate { get; set; }
         [Required]
-        [Display(Name = "Shipping Date")]
-        public DateTime ShippingDate { get; set; } 
-
+        public DateTime ShippingDate { get; set; }
+        [Required]
         public string Status { get; set; }
 
-        // stripe payment
-        public string? SessionId {
-            get;
-            set;
-        }
-
+        //stripe payment
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+
+        public string? Tracking { get; set; }
+        public string? Carrier { get; set; }
+
     }
 }
--- Tangy_Models/OrderHeaderDTO.cs
+++ Tangy_Models/OrderHeaderDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,7 +11,6 @@ namespace Tangy_Models
 {
     public class OrderHeaderDTO
     {
-        [Key]
         public int Id { get; set; }
 
         [Required]
@@ -23,16 +24,35 @@ namespace Tangy_Models
         public DateTime OrderDate { get; set; }
         [Required]
         [Display(Name = "Shipping Date")]
-        public DateTime ShippingDate { get; set; } 
-
+        public DateTime ShippingDate { get; set; }
+        [Required]
         public string Status { get; set; }
 
-        // stripe payment
-        public string? SessionId {
-            get;
-            set;
-        }
-
+        //stripe payment
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
+
+        [Display(Name = "Name")]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+        [Display(Name = "Email")]
+        [Required]
+        public string Email { get; set; }
+        public string? Tracking { get; set; }
+        public string? Carrier { get; set; }
+
     }
 }
